2a9d513107bffd3baa29146c31d5683bf9184766
hojunlee-hj <cajun7@naver.com>
2022-11-17 16:27:40 +0900

    Initial commit : make environment

---
+++ src/main/java/com/example/jwtWithOauth/JwtWithOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtWithOauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWithOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtWithOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtWithOauth/JwtWithOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtWithOauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
